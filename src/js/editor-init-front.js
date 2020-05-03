1382eb155fdc6e4d9f859d8208aa61abf792abfa
Aleksan <48214689+Aharonyan@users.noreply.github.com>
2020-05-03 11:48:17 +0300

    added style and js changes

--- src/js/editor-init-front.js
+++ src/js/editor-init-front.js
@@ -5,13 +5,25 @@ if ($("#bfe-editor-block")[0]) {
     /**
      * Saving button
      */
-    const saveButton = document.querySelectorAll('#save-editor-block')[0];
+    const post_title = document.querySelector('#post_title'),
+        saveButton = document.querySelectorAll('#save-editor-block')[0];
 
     /**
      * To initialize the Editor, create a new instance with configuration object
      * @see docs/installation.md for mode details
      */
     const bfe_page_editor = new BfeEditor();
+
+    /**
+     * on title click open caret to fist block
+     */
+    post_title.addEventListener("keyup", (event) => {
+        if (event.keyCode === 13) {
+            // Adding caret to fist block
+            bfe_page_editor.bfee_editor.caret.focus();
+            bfe_page_editor.bfee_editor.toolbar.open();
+        }
+    });
 
     /**
      * Saving example
