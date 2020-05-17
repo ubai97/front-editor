7e0ed120bfbc38ce0b5308ec190c0c502f856791
Aleksan <48214689+Aharonyan@users.noreply.github.com>
2020-05-17 13:13:42 +0300

    refactoring

--- front-editor.php
+++ front-editor.php
@@ -18,6 +18,8 @@
  * Version: 1.4
  */
 
+namespace BFE;
+
 // Exit if accessed directly
 defined('ABSPATH') || exit;
 
@@ -33,35 +35,46 @@ class BestFrontEndEditor
   public static function init()
   {
 
-    define('BFE_PLUGIN_URL', plugins_url('',__FILE__));
+    define('BFE_PLUGIN_URL', plugins_url('', __FILE__));
     define('BFE_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
     define('BFE_Template_PATH', plugin_dir_path(__FILE__) . 'templates/');
 
-    /**
-     * Add hook for activate plugin
-     */
-    register_activation_hook(__FILE__, function () {
-      do_action('BFE_activate');
-    });
+    register_activation_hook(__FILE__, [__CLASS__, 'fe_plugin_activate']);
 
-    register_deactivation_hook(__FILE__, function () {
-      do_action('BFE_deactivate');
-    });
+    register_deactivation_hook(__FILE__, [__CLASS__, 'fe_plugin_deactivate']);
 
     add_action('plugins_loaded', [__CLASS__, 'true_load_plugin_textdomain']);
-
 
     add_action('plugins_loaded', [__CLASS__, 'add_components']);
 
     add_filter('post_row_actions', [__CLASS__, 'add_link_to_edit_this_post'], 10, 2);
   }
 
+  /**
+   * On plugin activate
+   *
+   * @return void
+   */
+  public static function fe_plugin_activate()
+  {
+    do_action('BFE_activate');
+  }
+
+  /**
+   * On plugin deactivate
+   *
+   * @return void
+   */
+  public static function fe_plugin_deactivate()
+  {
+    do_action('BFE_deactivate');
+  }
+
+  /**
+   * Add Components
+   */
   public static function add_components()
   {
-
-    /**
-     * Add Components
-     */
     require_once __DIR__ . '/inc/MenuSettings.php';
     require_once __DIR__ . '/inc/Shortcodes.php';
     require_once __DIR__ . '/inc/PostsList.php';
@@ -89,15 +102,15 @@ class BestFrontEndEditor
    */
   public static function add_scripts()
   {
-    if (is_admin() ){
+    if (is_admin()) {
       return;
     }
 
-    if(!is_user_logged_in()){
+    if (!is_user_logged_in()) {
       return;
     }
 
-    if(is_page() || is_single() ) {
+    if (is_page() || is_single()) {
       wp_register_script('bfee-editor.js', BFE_PLUGIN_URL . '/assets/js/bfee-editor.js', array('jquery'), 2, true);
 
       wp_register_style(
@@ -121,7 +134,7 @@ class BestFrontEndEditor
   public static function add_link_to_edit_this_post($actions, $post)
   {
     if ($post->post_type == 'post') {
-      if ($edit_link = BFE\Editor::get_post_edit_link($post->ID)) {
+      if ($edit_link = Editor::get_post_edit_link($post->ID)) {
         $actions['bfe_front_editor_link'] = sprintf(
           '<a target="_blank" style="color:#388ffe;" href="%s">%s</a>',
           $edit_link,
