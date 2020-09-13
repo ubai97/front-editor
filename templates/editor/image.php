cff43b81dfcdcfa4080b2fed5fd0f1b73eccf9d0
Aleksan <48214689+Aharonyan@users.noreply.github.com>
2020-09-13 17:55:51 +0300

    #39 Pictures are not attached to the article fixed

--- templates/editor/image.php
+++ templates/editor/image.php
@@ -1,20 +1,39 @@
 <?php
-    $classes = array();
+$classes = array();
 
-    if ( !empty($data['stretched']) && $data['stretched'] == 'true'){
-        $classes[] = 'bfe-image--stretched';
-    }
+if (!empty($data['stretched']) && $data['stretched'] == 'true') {
+    $classes[] = 'bfe-image--stretched';
+}
 
-    if ( !empty($data['withBorder']) && $data['withBorder'] == 'true'){
-        $classes[] = 'bfe-image--bordered';
-    }
+if (!empty($data['withBorder']) && $data['withBorder'] == 'true') {
+    $classes[] = 'bfe-image--bordered';
+}
 
-    if ( !empty($data['withBackground']) && $data['withBackground'] == 'true'){
-        $classes[] = 'bfe-image--backgrounded';
-    }
+if (!empty($data['withBackground']) && $data['withBackground'] == 'true') {
+    $classes[] = 'bfe-image--backgrounded';
+}
 
-    $size_slug = "large";
+$size_slug = "large";
+
+$innerHTML = sprintf(
+    '<figure class="wp-block-image size-large"><img src="%s" alt="" class="wp-image-%s"/></figure>',
+    wp_get_attachment_image_url($image_id, 'large'),
+    $image_id
+);
+
+$block = [
+    'blockName' => 'core/image',
+    'attrs' =>
+    [
+        'id' => $image_id,
+        'sizeSlug' => $size_slug,
+    ],
+    'innerBlocks' => [
+        'innerHTML' => $innerHTML,
+    ],
+    'innerContent' => [$innerHTML]
+];
+
+echo serialize_block($block);
 ?>
-<!-- wp:image {"id":<?= $image_id ?>,"sizeSlug":<?= $size_slug ?>} -->
-<figure class="wp-block-image size-large"><img src="<?php echo wp_get_attachment_image_url( $image_id, 'large'); ?>"/></figure>
-<!-- /wp:image -->
+
